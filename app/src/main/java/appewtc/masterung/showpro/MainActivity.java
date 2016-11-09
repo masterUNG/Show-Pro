d35e8d4a4329c2f5371534017f559b4ebd5c9ef1
masterUNG <phrombutr@gmail.com>
2016-11-09 16:46:53 +0700

    Intent to ShoPro finish

--- app/src/main/java/appewtc/masterung/showpro/MainActivity.java
+++ app/src/main/java/appewtc/masterung/showpro/MainActivity.java
@@ -1,13 +1,58 @@
 package appewtc.masterung.showpro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView aisImageView, dtacImageView, trueImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        aisImageView = (ImageView) findViewById(R.id.imageView);
+        dtacImageView = (ImageView) findViewById(R.id.imageView2);
+        trueImageView = (ImageView) findViewById(R.id.imageView3);
+
+        //Image Controller
+        aisImageView.setOnClickListener(this);
+        dtacImageView.setOnClickListener(this);
+        trueImageView.setOnClickListener(this);
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+        int i = 0;
+
+        switch (view.getId()) {
+            case R.id.imageView:
+                i = 1;
+                break;
+            case R.id.imageView2:
+                i = 2;
+                break;
+            case R.id.imageView3:
+                i = 3;
+                break;
+        }   // switch
+
+        //Intent to ShowPro
+        Intent intent = new Intent(MainActivity.this, ShowPro.class);
+        intent.putExtra("Index", i);
+        startActivity(intent);
+
+
+
+    }   // onClick
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/showpro/ShowPro.java
+++ app/src/main/java/appewtc/masterung/showpro/ShowPro.java
@@ -2,12 +2,24 @@ package appewtc.masterung.showpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ShowPro extends AppCompatActivity {
+
+    //Explicit
+    private int anInt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_pro);
-    }
-}
+
+        //Get Value From Intent
+        anInt = getIntent().getIntExtra("Index", 1);
+        Log.d("9novV1", "Index ==> " + anInt);
+
+
+    }   // Main Method
+
+}   // Main Class
