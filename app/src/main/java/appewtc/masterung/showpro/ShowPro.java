2643fc93178e2be720d224059e45118949f2ca51
masterUNG <phrombutr@gmail.com>
2016-11-10 11:48:56 +0700

    Design UI ShowPro finish

--- app/src/main/java/appewtc/masterung/showpro/ShowPro.java
+++ app/src/main/java/appewtc/masterung/showpro/ShowPro.java
@@ -10,6 +10,7 @@ public class ShowPro extends AppCompatActivity {
     private int anInt;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
